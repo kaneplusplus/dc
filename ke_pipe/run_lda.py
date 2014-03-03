2cc60b83e95b15e836f403e2918121dc188ac3d5
Michael Kane <kaneplusplus@gmail.com>
2014-03-03 18:47:04 -0500

    Updated the run_lda script to work with tfidf's.

--- ke_pipe/run_lda.py
+++ ke_pipe/run_lda.py
@@ -5,16 +5,20 @@ import logging, gensim
 
 if __name__ == '__main__':
 
-  # load id->word mapping (the dictionary), one of the results of step 2 above
-  id2word = gensim.corpora.Dictionary.load('d.dict')
-  # load corpus iterator
-  mm = gensim.corpora.MmCorpus('corpus.mm')
-  tfidf = gensim.models.TfidfModel(mm)
-  corpus_tfidf = tfidf[mm]
-  # mm = gensim.corpora.MmCorpus(bz2.BZ2File('wiki_en_tfidf.mm.bz2')) # use this if you compressed the TFIDF output
+# load id->word mapping (the dictionary), one of the results of step 2 above
+id2word = gensim.corpora.Dictionary.load('d.dict')
+# load corpus iterator
+mm = gensim.corpora.MmCorpus('corpus_tfidf.mm')
+tfidf = gensim.models.TfidfModel(mm)
+tfidf.save("tfidf.model")
+corpus_tfidf = tfidf[mm]
+# mm = gensim.corpora.MmCorpus(bz2.BZ2File('wiki_en_tfidf.mm.bz2')) # use this if you compressed the TFIDF output
 
-  lda = gensim.models.ldamodel.LdaModel(corpus=corpus_tfidf, id2word=id2word, 
-    num_topics=6, update_every=0, passes=20)
+lda = gensim.models.ldamodel.LdaModel(corpus=corpus_tfidf, id2word=id2word, 
+  num_topics=6, update_every=0, passes=20)
 
-  for doc in mm:
-    print(lda[doc][0][0])
+lda.save("lda.model")
+topics = lda[corpus_tfidf]
+for t in topics:
+  memberships = [str(x[1]) for x in t]
+  print ",".join(memberships)
